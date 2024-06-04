059f9bac2ce104a73650a14b561c4e1f0cbb1bbc
HYUNSU4886 <167495966+HYUNSU4886@users.noreply.github.com>
2024-06-04 19:54:21 +0900

    feat: MxComponent 실습 및 Conveyor 프로그래밍

--- Assets/Scripts/Conveyor.cs
+++ Assets/Scripts/Conveyor.cs
@@ -7,33 +7,55 @@ public class Conveyor : MonoBehaviour
     public Transform Box;
     public Vector3 direction;
     public float speed;
+    public int isActive;
+    public int _direction;
+    public bool isBoxIn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isBoxIn = false;
+        _direction = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
+
+    public void OnConveyorBtnClkEvent()
+    {
+        isActive = 1;
+        StartCoroutine(Moving(direction,speed));
+    }
+    public void OffConveyorBtnClkEvent()
+    {
+        isActive = 0;
+    }
+
+    public void ReverseConveyorDirectionBtnClkEvent()
+    {
+        _direction = _direction * -1;
+    }
+
+
+    IEnumerator Moving(Vector3 direction, float speed)
+    {
+        while (isBoxIn)
+        {
+            Box.GetComponent<Rigidbody>().velocity = direction * speed * _direction;
+            if (isActive == 0)
+                break;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Box"))
         {
             Box = other.transform;
-            StartCoroutine(Moving(direction, speed));
-        }
-    }
-
-    IEnumerator Moving(Vector3 direction, float speed)
-    {
-        while (true)
-        {
-            Box.GetComponent<Rigidbody>().velocity = direction * speed;
-            yield return new WaitForSeconds(0.01f);
+            isBoxIn = true;
         }
     }
 
@@ -41,7 +63,7 @@ public class Conveyor : MonoBehaviour
     {
         if(other.gameObject.layer == LayerMask.NameToLayer("Box"))
         {
-            
+            isBoxIn = false;
         }
     }
 }
---
+++ MxComponentServer/MxComponentServer/Program.cs
@@ -0,0 +1,139 @@
+﻿using ActUtlType64Lib;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.InteropServices;
+
+namespace MxComponentServer
+{
+    enum State
+    {
+        Disconnected,
+        Connected
+    }
+
+    public class MxComopentServer
+    {
+        State state = State.Disconnected;
+        static ActUtlType64 mxComponent;
+
+        TcpListener listener;
+        TcpClient client;
+        NetworkStream stream;
+        Thread thread = new Thread(GetYDataBlock);
+        static string ydata;
+
+        public MxComopentServer()
+        {
+            mxComponent = new ActUtlType64();
+            mxComponent.ActLogicalStationNumber = 1;
+            StartTCPServer();
+
+            while (true)
+            {
+                int bytes;
+                byte[] buffer = new byte[100];
+                Console.WriteLine(stream.CanRead);
+                Console.WriteLine(stream.CanWrite);
+                if (stream.CanRead)
+                {
+                    stream.Read(buffer, 0, buffer.Length);
+                    string output = Encoding.ASCII.
+                    GetString(buffer, 0, 100).Trim('\0');
+                    Console.WriteLine(output);
+                    Console.WriteLine(stream.CanRead);
+                    Console.WriteLine(stream.CanWrite);
+                    buffer = Encoding.ASCII.GetBytes(output);
+                    stream.Write(buffer, 0, buffer.Length);
+
+                }
+            }
+        }
+
+        public string ConnectPLC()
+        {
+            int ret = mxComponent.Open();
+            if (ret == 0)
+            {
+                return "Connection succeded!";
+            }
+            else
+            {
+                return "Connection failed...";
+            }
+        }
+
+        public string DisconnectPLC()
+        {
+            int ret = mxComponent.Close();
+            if (ret == 0)
+            {
+                return "Disconnection succeded!";
+            }
+            else
+            {
+                return "Disconnection failed...";
+            }
+        }
+
+        static void GetYDataBlock()
+        {
+            short[] yData = new short[11];
+            mxComponent.ReadDeviceBlock2("Y0", 11, out yData[0]);
+
+            ydata = ConvertDataIntoString(yData);
+        }
+
+        public void SetData(string device, int value)
+        {
+            int ret = mxComponent.SetDevice(device, 1);
+        }
+        static string ConvertDataIntoString(short[] data)
+        {
+            string newYData = "";
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == 0)
+                {
+                    newYData += "0000000000";
+                    continue;
+                }
+
+                string temp = Convert.ToString(data[i], 2);// 100
+                string temp2 = new string(temp.Reverse().ToArray()); // reverse 100 -> 001  
+                newYData += temp2; // 0000000000 + 001
+
+                if (temp2.Length < 10)
+                {
+                    int zeroCount = 10 - temp2.Length; // 7 -> 7개의 0을 newYData에 더해준다. (0000000)
+                    for (int j = 0; j < zeroCount; j++)
+                        newYData += "0";
+                } // 0000000000 + 001 + 0000000 -> 총 20개의 비트
+            }
+
+            return newYData;
+        }
+
+        void StartTCPServer()
+        {
+            listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+
+            client = listener.AcceptTcpClient();
+            stream = client.GetStream();
+            Console.WriteLine("Start TCP Server");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MxComopentServer server = new MxComopentServer();
+        }
+    }
+
+}
